93c43a0a79ce2473ffa6852c3864ba40054a514b
ricaramx77 <ricaramx77@outlook.com>
2026-03-17 17:11:52 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Docker_CICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
